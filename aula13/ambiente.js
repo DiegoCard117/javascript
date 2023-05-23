078618d2f71102bfaf89735f78f9546736368a65
DiegoCard117 <diegocardoso117@gmail.com>
2023-05-23 20:01:47 -0300

    while

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+// console.log('Tudo bem?')
+
+
+let c = 1
+/*
+do {
+    console.log(`${c}`)
+    c++
+} while (c <= 6)
+
+
+while (c <= 6) {
+    console.log(`${c}`)
+    c++
+}
+*/
+
+while (c < 10) {
+    console.log('te amo vida')
+    c++
+    
+}
