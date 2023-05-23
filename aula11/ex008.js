473ed674017f51af76537bad1bcc0d1a6f194935
DiegoCard117 <diegocardoso117@gmail.com>
2023-05-23 01:07:58 -0300

    velocidade e nacionalidade

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 40.5
+if (vel > 50) {
+    console.log(`Voce ganhou uma multa por estar a ${vel} em uma via de 50`)
+} else {
+    console.log(`Sua velocidade é ${vel}, continue dirigindo com segurança`)
+}
