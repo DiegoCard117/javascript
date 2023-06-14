3587245cc2fb65bfdddca3c6e5406f77e9c04ea0
DiegoCard117 <diegocardoso117@gmail.com>
2023-06-13 21:46:20 -0300

    estudos js

--- estudos independentes/arrays.js
+++ estudos independentes/arrays.js
@@ -1,21 +1,38 @@
+//Array
 let nome = ["Yasmin", "Ray", "Diego"]
 nome.push("Marcio")
 nome.sort()
 console.log(typeof(nome))
 console.log(nome)
 
-/*
+
 for(let i = 0;i < nome.length; i++){
     console.log(nome[i])
 }
 
+/*
 let i = 0
 while (i < nome.length) {
     console.log(nome[i++])
 }
-*/
+
 
 //mesmo de cima mas usando callback
 nome.map(function(n){
     console.log(n)
 })
+
+//obj
+var pessoa = {
+    nome: "Diego",
+    idade: 21,
+    profissao: "Dev"
+}
+
+console.log(pessoa.idade)
+
+pessoa.tamanho = 1.89
+
+console.log(pessoa)
+
+*/
--- estudos independentes/funçoes.js
+++ estudos independentes/funçoes.js
@@ -1,4 +1,6 @@
+/*
 //funçao vazia
+console.log("-----------------")
 function somar() {
     let n = 20
     n++
@@ -6,15 +8,14 @@ function somar() {
 }
 
 somar()
-
+console.log("-----------------")
 //funçao com parametro
 function soma(n1, n2) {
     let total = n1 + n2
-    console.log(`funçao somar() da ${total}`)
+    console.log(`${n1} + ${n2} = ${total}`)
 }
-
 soma(3, 6)
-
+console.log("-----------------")
 //return
 function multiplicar(num1, num2) {
     let total = num1 * num2
@@ -23,6 +24,7 @@ function multiplicar(num1, num2) {
 
 let multi = multiplicar(6, 8)
 console.log(`funçao multi() da ${multi}`)
+console.log("-----------------")
 
 //arrow
 //variavel dividir recebe funçao
@@ -31,11 +33,13 @@ const dividir = (numero1, numero2) => {
     return divisao
 }
 
+console.log("-----------------")
 let divi = dividir(10, 2)
 console.log(`funçao dividir() da ${divi}`)
-
+console.log("-----------------")
 //as duas funçoes sao iguais mas a debaixo é resumida
 const dividir2 = (numero1, numero2) => numero1 / numero2
 
 let divi2 = dividir(10, 2)
 console.log(`funçao dividir2() da ${divi2}`) 
+*/
