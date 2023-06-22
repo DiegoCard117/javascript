bd20b8b59af7c8cae1b6b54f026f0e8b460fabf6
DiegoCard117 <diegocardoso117@gmail.com>
2023-06-22 15:21:27 -0300

    Update aula56.js

--- Udemy-js/aula56.js
+++ Udemy-js/aula56.js
@@ -13,9 +13,10 @@ function criaBmw(modelo, ano) {
 
 const Bmw1 = criaBmw('M3', 2020)
 
+/*
 console.log(Bmw1)
 console.log(Bmw1.ligar())
-
+*/
 
 //factory de um perfil de pessoas
 function criaPessoa(nome, sobrenome, altura, peso) {
@@ -27,12 +28,28 @@ function criaPessoa(nome, sobrenome, altura, peso) {
         fala(assunto){
             return `${this.nome} está ${assunto}`
         },
-        get imc(){  // getter "finge ser um atributo do objeto"
+        //getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+        //setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ')
+            this.nome = valor.shift()
+            this.sobrenome = valor.join(' ')
+            console.log(valor)
+
+        },
+
+        get imc(){  // getter "finge ser um atributo do objeto" nao usasse () quando for chamar
             return (this.peso / (this.altura ** 2)).toFixed(2)
         }
     }
 }
 
 const pessoa1 = criaPessoa ('Diego', 'Cardoso', 1.9, 83)
-console.log(pessoa1)
+//console.log(pessoa1)
 console.log(pessoa1.imc)
+pessoa1.nomeCompleto = 'Ray Alcidio Cardoso Chagas'
+console.log(pessoa1.nome)
+console.log(pessoa1.sobrenome)
