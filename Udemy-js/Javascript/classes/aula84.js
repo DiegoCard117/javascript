6fd02320ab59b3db6c87d492eb51782d6587d401
DiegoCard117 <diegocardoso117@gmail.com>
2023-06-30 11:29:30 -0300

    refatorando para Classes

--- Udemy-js/Javascript/classes/aula84.js
+++ Udemy-js/Javascript/classes/aula84.js
@@ -3,4 +3,20 @@ class ControleRemoto {
         this.tv = tv
         this.volume = 0
     }
+    //instacia
+    aumentarVolume() {
+        this.volume++
+        console.log(this.volume)
+    }
+    //estatico -- nao tenho "tv e nome"
+    static Switch() {
+       console.log('troquei')
+    }
 }
+
+const control = new ControleRemoto('samsung') 
+console.log(control)
+control.aumentarVolume()
+console.log(control)
+
+ControleRemoto.Switch()
